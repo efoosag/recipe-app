7e5a3436e334b7cc4a26a7bb92864cb91632ff23
efoosag <efoosagsite@gmail.com>
2022-10-20 09:08:40 +0100

    fix conflict

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,7 +7,6 @@ class FoodsController < ApplicationController
 
   def new
     @food = Food.new
-<<<<<<< HEAD
     @current_user = current_user
   end
 
@@ -24,23 +23,11 @@ class FoodsController < ApplicationController
     else
       flash[:alert] = 'Price must be greater than 0'
       redirect_to new_food_url
-=======
-  end
-
-  def create
-    @food = Food.new(food_params)
-    if @food.save
-      redirect_to foods_path
-    else
-      render 'new'
->>>>>>> 205037de58e1c879066dad423d45d4d6b36df6ac
     end
   end
 
   def destroy
     @food = Food.find(params[:id])
-<<<<<<< HEAD
-
     @check_food = RecipeFood.where(food: @food)
     if @check_food.count.positive?
       flash[:alert] = "#{@food.name} belongs to some recipes"
@@ -53,15 +40,5 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-=======
-    @food.destroy
-    redirect_to foods_path
-  end
-
-  private
-
-  def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price)
->>>>>>> 205037de58e1c879066dad423d45d4d6b36df6ac
   end
 end
